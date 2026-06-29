1320f02b61a588efe2c089b1bb3b728a85be21ac
Sang Nguyen <vsang25@gmail.com>
2026-06-29 13:31:46 +0700

    chore: update iOS/ReactNative configs, fix MapView manager, add CPP exception handler

--- demo/MapVina/Constants.swift
+++ demo/MapVina/Constants.swift
@@ -13,9 +13,9 @@ struct Constants {
     static let baseurlSG = "https://sg-maps.mapvina.com/"
     static let baseurlTH = "https://th-maps.mapvina.com/"
     
-    static let urlStyleVN = "https://maps.mapvina.com/styles/v1/streets.json?key=public"
-    static let urlStyleSG = "https://sg-maps.mapvina.com/styles/v1/streets.json?key=public"
-    static let urlStyleTH = "https://th-maps.mapvina.com/styles/v1/streets.json?key=public"
+    static let urlStyleVN = "https://maps.mapvina.com/styles/v2/streets.json?key=public_key"
+    static let urlStyleSG = "https://sg-maps.mapvina.com/styles/v2/streets.json?key=public_key"
+    static let urlStyleTH = "https://th-maps.mapvina.com/styles/v2/streets.json?key=public_key"
     
     static let urlStyle3DVN = "https://tiles.mapvina.com/sats/v1/satellite/satellite.json?key=public"
     static let urlStyle3DSG = "https://sg-tiles.mapvina.com/sats/v1/satellite/satellite.json?key=public"
--- demo/MapVina/server/AppDelegate.swift
+++ demo/MapVina/server/AppDelegate.swift
@@ -6,13 +6,80 @@
 //
 
 import Foundation
-
 import UIKit
 import GoogleMaps
+import MapVina
+
+// NSURLProtocol that intercepts MapVina requests and forces HTTP/1.1
+// by using NSURLConnection (which doesn't support QUIC)
+class HTTP1Protocol: URLProtocol {
+    static var propertyKey = "HTTP1ProtocolHandled"
+
+    override class func canInit(with request: URLRequest) -> Bool {
+        guard let scheme = request.url?.scheme?.lowercased(),
+              scheme == "https" || scheme == "http" else { return false }
+        guard let host = request.url?.host?.lowercased(),
+              host.contains("mapvina.com") else { return false }
+        if URLProtocol.property(forKey: propertyKey, in: request) != nil {
+            return false
+        }
+        return true
+    }
+
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        request
+    }
+
+    override func startLoading() {
+        var newRequest = (request as NSURLRequest).mutableCopy() as! NSMutableURLRequest
+        URLProtocol.setProperty(true, forKey: HTTP1Protocol.propertyKey, in: newRequest)
+
+        DispatchQueue.global().async { [weak self] in
+            guard let self = self else { return }
+            var response: URLResponse?
+            do {
+                let data = try NSURLConnection.sendSynchronousRequest(
+                    newRequest as URLRequest,
+                    returning: &response
+                )
+                if let httpResponse = response as? HTTPURLResponse {
+                    DispatchQueue.main.async {
+                        self.client?.urlProtocol(self, didReceive: httpResponse, cacheStoragePolicy: .notAllowed)
+                        self.client?.urlProtocol(self, didLoad: data)
+                        self.client?.urlProtocolDidFinishLoading(self)
+                    }
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    self.client?.urlProtocol(self, didFailWithError: error)
+                }
+            }
+        }
+    }
+
+    override func stopLoading() {}
+}
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         GMSServices.provideAPIKey("AIzaSyDEYfN5At0Qyp5KCDhBTUaeBxYUqG-gOds")
+
+        // Register HTTP1Protocol in MLNNetworkConfiguration to force HTTP/1.1
+        // This fixes QUIC/HTTP3 timeout in the iOS simulator
+        let sessionConfig = URLSessionConfiguration.default
+        sessionConfig.timeoutIntervalForResource = 30
+        sessionConfig.httpMaximumConnectionsPerHost = 8
+        sessionConfig.requestCachePolicy = .reloadIgnoringLocalCacheData
+        sessionConfig.urlCache = nil
+        var protocols = sessionConfig.protocolClasses ?? []
+        protocols.insert(HTTP1Protocol.self, at: 0)
+        sessionConfig.protocolClasses = protocols
+        MLNNetworkConfiguration.sharedManager.sessionConfiguration = sessionConfig
+        NSLog("AppDelegate: registered HTTP1Protocol in MLNNetworkConfiguration")
+
+        MLNSettings.use(.mapVina)
+        MLNSettings.apiKey = "public_key"
+
         return true
     }
 }
